6132ec4ea95ccbf7b7576c8429aab965946ce454
Omkar Khamkar <omkarkhamkar2004@gmail.com>
2025-04-01 13:52:25 +0530

    Update snowpack.config.mjs

--- Frontend/snowpack.config.mjs
+++ Frontend/snowpack.config.mjs
@@ -1,21 +1,29 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    src: "/dist/",
-    public: "/",
+    src: "/src/",  // Mount the 'src' folder
+    public: "/",   // Public folder for hot reload
   },
-  plugins: [],
+  plugins: ['@snowpack/plugin-react-refresh'], // React hot refresh plugin
   routes: [
     // Enable an SPA Fallback in development
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {
+      match: "routes",
+      src: ".*",
+      dest: "/index.html",
+    },
   ],
-  optimize: {},
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2020',  // Settings for bundling and optimization
+  },
   packageOptions: {},
   devOptions: {
-    port: 8080,
+    port: 8080,  // Development server port
   },
   buildOptions: {},
   env: {
-    SNOWPACK_PUBLIC_API: "http://localhost:3001",
+    SNOWPACK_PUBLIC_API: "http://localhost:3001",  // API URL
   },
 };
