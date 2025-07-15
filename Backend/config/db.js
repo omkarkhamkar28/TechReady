087d5630e47fda37861c97745081b903ec6388b4
Omkar Khamkar <omkarkhamkar2004@gmail.com>
2025-07-15 22:01:47 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,10 @@ dotenv.config();
 const con = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`connected To Mongodb database ${conn.connection.host}`);
     } catch (error) {
+        console.error(error.message);
+        console.log("Database connecting error.")
         process.exit();
     }
 }
